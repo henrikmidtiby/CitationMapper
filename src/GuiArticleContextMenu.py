c7e48e5e5d72a5b86130488623a473263f89878c
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2026-03-18 12:49:00 +0100

    Removed dead code

--- src/GuiArticleContextMenu.py
+++ src/GuiArticleContextMenu.py
@@ -35,22 +35,6 @@ class GuiArticleContextMenu:
         print(data)
         return False
 
-    def show_context_menu_old(self, widget, data, event):
-        menu = gtk.Menu()
-        one = gtk.MenuItem("One")
-        menu.append(one)
-        submenu = gtk.Menu()
-        two = gtk.MenuItem("Two")
-        two.connect("activate", self.hello)
-        submenu.append(two)
-        one.set_submenu(submenu)
-        three = gtk.MenuItem("Three")
-        three.connect("activate", self.hello)
-        menu.append(three)
-        menu.show_all()
-        menu.popup(None, None, None, event.button, event.get_time())
-        return True
-
     def show_context_menu(self, widget, data, event):
         menu = gtk.Menu()
         one = gtk.MenuItem("Add to ban list")
